8dc21835b7055bf28cca2448e8c1cb116b78a600
Shane Osbourne <shane.osbourne8@gmail.com>
2021-04-30 20:01:40 +0100

    multi ops

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import "./algo-stack.lit";
 import Stack from "./algo-stack.lit";
 import {Controls, name} from "./controls.lit";
 import {Action} from "./cell.lit";
+import {balanced, Val} from "./algos";
 
 console.log('register %O', name);
 
 const algoInput = document.getElementById('algo-input') as Stack;
 const algoStack = document.getElementById('algo-stack') as Stack;
 const algoAction = document.getElementById('algo-action') as Action;
-const indexI = document.querySelector('[data-var-i]');
+
 const elems = {
   POINTER: '[data-pointer]'
 }
+
 const colors = {
   SELECTED: '#aed049',
   DEFAULT: '#ffffff'
@@ -23,15 +25,9 @@ const master = gsap.timeline();
 const stackTimeline = gsap.timeline();
 const DURATION = 0.3;
 
-type Op = { name: "push" | "pop", value?: string }
-type Val = { index: number, ops: Op[], stack: string[] }
-const values: Val[] = [
-  {index: 0, stack: [')'], ops: [{name: 'push', value: ')'}]},
-  {index: 1, stack: [')', ')'], ops: [{name: 'push', value: ')'}]},
-  {index: 2, stack: [')'], ops: [{name: 'pop'}]},
-  {index: 3, stack: [], ops: [{name: 'pop'}]},
-];
-const input = "(())";
+const input = "()[]{}";
+const res = balanced(input);
+res.values.forEach(v => console.log(v));
 
 function pointer(inputs: Val[]) {
   const cells = algoInput.cells();
@@ -47,7 +43,7 @@ function pointer(inputs: Val[]) {
     .fromTo(elems.POINTER, {scale: 0, duration: DURATION}, {scale: 1, duration: DURATION})
 
   inputs.forEach((val, index) => {
-    const op = val.ops[0] as Op;
+    const op = val.ops[0];
     timeline
       .to(elems.POINTER, {
           translateX: val.index * 40,
@@ -56,56 +52,58 @@ function pointer(inputs: Val[]) {
           ease: 'release',
         },
       )
-      .to(cells[index], {color: colors.SELECTED, scale: 1.5, duration: DURATION})
-      .call(() => {
-        const op = val.ops[0];
-        if (!op) return;
-        if (!algoAction) return;
-        switch (op.name) {
-          case "push": {
-            algoAction.action = `stack.push("${op.value}")`
-            break;
-          }
-          case "pop": {
-            algoAction.action = `stack.pop()`
-            break;
-          }
-        }
-      })
-      .fromTo(algoAction, {
-        scale: 0.8,
-        translateY: -20,
-        opacity: 0,
-        duration: DURATION
-      }, {
-        scale: 1,
-        translateY: 0,
-        opacity: 1,
-        duration: DURATION,
-      })
-      .addLabel("action-in")
-      .to(algoAction, {
-        scale: 0.8,
-        translateY: 20,
-        opacity: 0,
-        duration: DURATION
-      }, "+=1")
-      .to(cells[index], {color: colors.DEFAULT, scale: 1, duration: DURATION})
+      .to(cells[index], {color: colors.SELECTED, scale: 1.5, duration: DURATION});
+
+      if (op && op.name) {
+
+        timeline.call(() => {
+          const strings = val.ops.map(op => {
+            switch (op.name) {
+              case "push": return `stack.push("${op.value}")`
+              case "pop": return `stack.pop()`
+              case "cmp": return `${op.values.lhs} === ${op.values.rhs}`
+            }
+          }).join(', ');
+          algoAction.action = strings;
+        })
+        .fromTo(algoAction, {
+          scale: 0.8,
+          translateY: -20,
+          opacity: 0,
+          duration: DURATION
+        }, {
+          scale: 1,
+          translateY: 0,
+          opacity: 1,
+          duration: DURATION,
+        })
+        .addLabel("action-in")
+        .to(algoAction, {
+          scale: 0.8,
+          translateY: 20,
+          opacity: 0,
+          duration: DURATION
+        }, "+=1")
+      }
+    timeline.to(cells[index], {color: colors.DEFAULT, scale: 1, duration: DURATION})
 
     timeline
       .call(() => {
         const stack = val.stack;
+        if (!op) return;
         if (op.name === "push") {
           algoStack.setStack(stack);
         }
         if (op.name === "pop") {
           const lastCell = algoStack.lastCell();
           // const last = algoStack.lastCellSpan();
-          stackTimeline
-            .to(lastCell!, {opacity: 0, scale: 0, duration: DURATION})
-            .call(() => {
-              algoStack.setStack(stack);
-            })
+          if (lastCell) {
+            stackTimeline
+              .to(lastCell!, {opacity: 0, scale: 0, duration: DURATION})
+              .call(() => {
+                algoStack.setStack(stack);
+              })
+          }
         }
       }, [], "action-in+=0.2")
       .addLabel('after-action-in')
@@ -113,36 +111,23 @@ function pointer(inputs: Val[]) {
     timeline
       .call(() => {
         const last = algoStack.lastCell();
-        if (last && op.name === "push") {
+        if (last && op && op.name === "push") {
           stackTimeline.fromTo(last!, {opacity: 0, scale: 0, duration: DURATION}, {opacity: 1, scale: 1, duration: DURATION});
         }
       }, [], "after-action-in-=1")
   })
 
+  timeline.call(() => {
+    algoAction.action = `result: ${res.result}`
+  })
+    .to(algoAction, {opacity: 1, translateY: 0, duration: DURATION})
+
   return timeline;
 }
 
-function updateI(num: number | string) {
-  if (indexI) {
-    indexI.textContent = `i = ${String(num)}`
-  }
-}
-
 algoInput.stack = input.split('');
+
 setTimeout(() => {
   master
-    .add(pointer(values))
+    .add(pointer(res.values))
 }, 0);
-
-
-window.__pause = function () {
-  master.pause()
-}
-window.__play = function () {
-  master.play()
-}
-window.__restart = function () {
-  master.restart()
-  // clearStack();
-  updateI(0)
-}
